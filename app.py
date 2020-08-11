7b30af6b842fee3757c5f95249910352d127e402
SHAKILEASH <shakileash2000@gmail.com>
2020-08-11 10:49:23 +0530

    Sent image from flask to template

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import flask
+from PIL import Image
+import os , io , sys
+import numpy as np 
+import cv2
+import base64
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+
+imageData = []
 
 @app.route("/")
 def Welcome():
@@ -8,9 +19,27 @@ def Welcome():
 
 @app.route('/imageFilters/',methods=['GET','POST'])
 def upload():
+    global imageData
     imagefile = flask.request.files.get('file') 
-    print(imagefile)    
+    imageData = imagefile.read()
+    #print(imageData,"file data")    
     return render_template("filters.html",user_image = imagefile) 
+
+@app.route('/maskImage' , methods=['POST'])
+def mask_image():
+    #file = request.files['image'].read()
+    global imageData
+    npimg = np.frombuffer(imageData, np.uint8)
+    img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    #print("runnnnnnnnnnn")
+    img = Image.fromarray(img.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes,"JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return jsonify({'status':str(img_base64)})
+
 
 
 if __name__ == '__main__':
