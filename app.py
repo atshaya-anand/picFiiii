888186732fedbdbfaa2e96177019de021a14e294
SHAKILEASH <shakileash2000@gmail.com>
2020-08-13 18:09:16 +0530

    made filters global connected with ajax

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ CORS(app)
 
 
 imageData = [] #image variable to hold the user selecte image
+appliedFilterdata = [] #image variable to hold the  filter
 
 @app.route("/")
 def Welcome():
@@ -31,32 +32,55 @@ def upload():
 
 @app.route('/applyBasics',methods=['GET','POST'])
 def applyBasics():
-    global imageData
+    global imageData,appliedFilterdata
     if request.method == "POST":
         filter = request.form['basic']
         if filter == 'Sharpen':
             kernal = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
             sharpened_img = cv2.filter2D(imageData,-1,kernal)
+            appliedFilterdata = sharpened_img
             return render_template("output.html")
         elif filter == 'Blur':
             kernal = np.array([[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]])
             blurred_img = cv2.filter2D(imageData,-1,kernal)
+            appliedFilterdata = blurred_img
             return render_template("output.html")
         elif filter == 'Outline':
             kernal = np.array([[-1,-1,-1],[-1,8-1],[-1,-1,-1]])
             outlined_img = cv2.filter2D(imageData,-1,kernal)
+            appliedFilterdata = outlined_img
             return render_template("output.html")
         elif filter == 'Emboss':
             kernal = np.array([[-2,-1,0],[-1,1,1],[0,1,2]])
             embossed_img = cv2.filter2D(imageData,-1,kernal)
+            appliedFilterdata = embossed_img
             return render_template("output.html")
         elif filter == 'Custom':
             kernal = np.array([[0,0,0],[0,1,0],[0,0,0]])
             custom_img = cv2.filter2D(imageData,-1,kernal)
+            appliedFilterdata = custom_img
             return render_template("output.html")
         elif filter == 'Black & White':
             gray_img = cv2.cvtColor(imageData,cv2.COLOR_RGB2GRAY)
+            appliedFilterdata = gray_img
             return render_template("output.html")
+
+        
+@app.route('/backTofilters',methods=['GET'])
+def redirectTofilter():
+    return render_template("filters.html")
+
+
+@app.route('/basicfilterImage' , methods=['POST'])
+def render_image():
+    global appliedFilterdata
+    #imageData = np.array(imageData)
+    img = Image.fromarray(appliedFilterdata.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes,"JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return jsonify({'status':str(img_base64)})
 
 
 @app.route('/maskImage' , methods=['POST'])
