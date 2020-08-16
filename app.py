2c448892b2377289999efff7b8deb499bf02a394
Atshaya <atshaya1699@gmail.com>
2020-08-16 12:25:00 +0530

    again added changes

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def mask_image():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port=5000,debug=True,use_reloader=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
