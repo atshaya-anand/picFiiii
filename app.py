708ac29296ef9159996168927bc00a5fedd8c082
SHAKILEASH <shakileash2000@gmail.com>
2020-10-10 21:05:29 +0530

    user session handeled

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import cv2
 import base64
 from flask_cors import CORS
 from uuid import uuid4
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -36,8 +37,19 @@ def upload():
     imageData = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) 
 
     token = uuid4()
-    session[str(token)] = {"imageData":imageData,"appliedFilterdata":[]}
+    session[str(token)] = {"imageData":imageData,"appliedFilterdata":[],"Time":time.time()}
     #print(session)
+
+    now = time.time()
+    keys = session.keys()
+    list_keys = []
+    for key in keys:
+        time_diff = now - session[key]["Time"]
+        if int(time_diff) >  180:
+            list_keys.append(key)
+
+    for i in list_keys:
+        del session[i]
 
     return render_template("filters.html",token=str(token)) 
 
@@ -47,37 +59,48 @@ def applyBasics():
     global imageData,appliedFilterdata
     if request.method == "POST":
         filter = request.form['basic']
-        token = request.form['token']
-        imageData = session[token]["imageData"]
+        try:
+            token = request.form['token']
+            imageData = session[token]["imageData"]
+        except:
+            return render_template("index.html")
+
+        session[token]["Time"] = time.time()
         print(request.form)
         if filter == 'Sharpen':
             kernal = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
             sharpened_img = cv2.filter2D(imageData,-1,kernal)
             appliedFilterdata = sharpened_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         elif filter == 'Blur':
             kernal = np.array([[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]])
             blurred_img = cv2.filter2D(imageData,-1,kernal)
             appliedFilterdata = blurred_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         elif filter == 'Outline':
             kernal = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
             outlined_img = cv2.filter2D(imageData,-1,kernal)
             appliedFilterdata = outlined_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         elif filter == 'Emboss':
             kernal = np.array([[-2,-1,0],[-1,1,1],[0,1,2]])
             embossed_img = cv2.filter2D(imageData,-1,kernal)
             appliedFilterdata = embossed_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         elif filter == 'Custom':
             kernal = np.array([[0,0,0],[0,1,0],[0,0,0]])
             custom_img = cv2.filter2D(imageData,-1,kernal)
             appliedFilterdata = custom_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         elif filter == 'Black & White':
             gray_img = cv2.cvtColor(imageData,cv2.COLOR_RGB2GRAY)
             appliedFilterdata = gray_img
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
 
 @app.route('/applyAdvance',methods=['GET','POST'])
@@ -85,27 +108,36 @@ def applyAdvance():
     global imageData,appliedFilterdata
     if request.method == "POST":
         filter = request.form['advance']
-        token = request.form['token']
-        imageData = session[token]["imageData"]
+        try:
+            token = request.form['token']
+            imageData = session[token]["imageData"]
+        except:
+            return render_template("index.html")    
+        session[token]["Time"] = time.time()
         if filter == 'Cartoonification':
             cartoon_image = cv2.stylization(imageData, sigma_s=150, sigma_r=0.25)  
             appliedFilterdata = cartoon_image
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         if filter == 'Pencil Sketch':
             dst_gray, dst_color = cv2.pencilSketch(imageData, sigma_s=60, sigma_r=0.07, shade_factor=0.05)   
             appliedFilterdata = dst_gray
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         if filter == 'Coloured Pencil Sketch':
             dst_gray, dst_color = cv2.pencilSketch(imageData, sigma_s=60, sigma_r=0.07, shade_factor=0.05)   
             appliedFilterdata = dst_color
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         if filter == 'Oil Paint':
             dst = cv2.xphoto.oilPainting(imageData, 7, 1)
             appliedFilterdata = dst
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
         if filter == 'Water Colour':
             res = cv2.stylization(imageData, sigma_s=60, sigma_r=0.6)
             appliedFilterdata = res
+            session[token]["appliedFilterdata"] = appliedFilterdata
             return render_template("output.html",token=token)
 
         
@@ -113,7 +145,12 @@ def applyAdvance():
 def redirectTofilter():
     try:
         print(request.form)
-        token = request.form["token"]
+        try:
+            token = request.form["token"]
+        except:
+            return render_template("index.html")
+
+        session[token]["Time"] = time.time()
         return render_template("filters.html",token = token)
     except:
         return render_template("index.html")    
@@ -123,6 +160,12 @@ def redirectTofilter():
 def render_image():
     global appliedFilterdata
     #imageData = np.array(imageData)
+    token = request.data.decode("utf-8") 
+    print(token)
+    try:
+        appliedFilterdata = session[token]["appliedFilterdata"]
+    except:
+        return render_template("index.html")      
     img = Image.fromarray(appliedFilterdata.astype("uint8"))
     rawBytes = io.BytesIO()
     img.save(rawBytes,"JPEG")
@@ -137,7 +180,10 @@ def mask_image():
     #imageData = np.array(imageData)
     token = request.data.decode("utf-8") 
     print(token)
-    imageData = session[token]["imageData"]
+    try:
+        imageData = session[token]["imageData"]
+    except:
+        return render_template("index.html")  
     img = Image.fromarray(imageData.astype("uint8"))
     rawBytes = io.BytesIO()
     img.save(rawBytes,"JPEG")
